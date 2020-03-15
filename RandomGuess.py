d4812d7c313b1aa3303507211d727f0aa186ee89
Ruby Diehl <diehl@pdx.edu>
2020-03-15 11:51:23 -0700

    still figuring git out...again

--- RandomGuess.py
+++ RandomGuess.py
@@ -1,6 +1,6 @@
 from sklearn.metrics import accuracy_score
 from math import sqrt
-from useful_components import TwitterDataSet
+from useful_components import TwitterDataSet, save_persistant_model
 from sklearn.dummy import DummyClassifier
 
 
@@ -10,13 +10,14 @@ def random_guess():
     # split data set for training and testing
     X_train, X_test, y_train, y_test = data_set.get_test_train_split()
 
-    n_features = data_set.get_num_features()
-
-    max_feat = int(sqrt(n_features))
     classifier = DummyClassifier(strategy="uniform", random_state=0)
     classifier.fit(X_train, y_train)
     y_pred = classifier.predict(X_test)
 
     print(accuracy_score(y_test, y_pred))
+
+    vec_path = 'RandomGuess_persistent_model/vectorizer.joblib'
+    model_path = 'RandomGuess_persistent_model/model.joblib'
+    save_persistant_model(vec_path, model_path, classifier, data_set)
 
 random_guess()
